a49bb1697eb094b86a564de9a5b5b56476368604
Aayushi <120032104+Aayushi2104@users.noreply.github.com>
2026-02-20 11:54:22 +0530

    persistence

--- src/main/java/com/lpu/App.java
+++ src/main/java/com/lpu/App.java
@@ -1,11 +1,14 @@
 package com.lpu;
 
 import com.lpu.entity.Person;
+import config.CustomClassConfiguration;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import org.hibernate.jpa.HibernatePersistenceProvider;
 
 import javax.swing.text.html.parser.Entity;
+import java.util.HashMap;
 
 /**
  * Hello world!
@@ -15,8 +18,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        EntityManagerFactory entityManagerFactory= Persistence.createEntityManagerFactory("dev");
+//        System.out.println( "Hello World!" );
+//        EntityManagerFactory entityManagerFactory= Persistence.createEntityManagerFactory("dev");
+        HashMap<Object,Object>props=new HashMap<>();
+        props.put("hibernate.hbm2ddl.auto","create");
+        props.put("hibernate.show_sql","true");
+        EntityManagerFactory entityManagerFactory=new HibernatePersistenceProvider().
+                createContainerEntityManagerFactory(new CustomClassConfiguration(), new HashMap<>());
         EntityManager em=entityManagerFactory.createEntityManager();
         em.getTransaction().begin();
         Person person=new Person();
